0dad30bc768da4a1512b6b8ddedac842103e6859
sandeep21r <100214644+sandeep21r@users.noreply.github.com>
2025-03-24 15:55:41 +0530

    first commit

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, Wrld!"
 
 if __name__ == "__main__":
     # Run in debug mode for local development
--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ def test_hello():
     with app.test_client() as client:
         response = client.get("/")
         assert response.status_code == 200
-        assert response.data == b"Hello, World!"
+        assert response.data == b"Hello, Wrld!"
